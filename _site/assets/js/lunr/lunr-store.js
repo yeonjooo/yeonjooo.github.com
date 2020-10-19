a5a603a07d462709c9b5f109ecd0b432d51739bd
root <root@localhost.localdomain>
2020-10-19 22:53:39 +0900

    Update jekyll theme.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Welcome to Yeonjoo's blog!",
+        "excerpt":"Hello!  ","categories": ["test"],
+        "tags": [],
+        "url": "http://0.0.0.0:4000/test/test/",
+        "teaser": null
+      }]
