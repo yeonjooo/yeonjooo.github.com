be14cd8a0cd483b7f7720576df39e78d1914c260
yeonjooo <yeonjooo13@gmail.com>
2020-10-21 22:17:16 +0900

    Update TIL

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,19 @@
 var store = [{
-        "title": "Welcome to Yeonjoo's blog!",
-        "excerpt":"Hello!  ","categories": ["test"],
+        "title": "[TIL] 2020-10-19",
+        "excerpt":"   github blog 생성            jekyll 적용       docker로 test 환경 구축                 docker run –rm –volume=”$PWD:/srv/jekyll” –privileged -p 4000:4000 -it jekyll/jekyll /bin/bash     jekyll serve                    접속할 수 있는 주소를 출력해주면 해당 url로 접속하여 테스트 후 push                            docker로 테스트 환경 구축하면 매우 편리               github은 내장하고 있는 ruby 엔진을 통해 로딩                                                   ","categories": ["TIL"],
+        "tags": ["TIL"],
+        "url": "http://0.0.0.0:4000/til/TIL/",
+        "teaser": null
+      },{
+        "title": "[TIL] 2020-10-20",
+        "excerpt":"   CLion에서 github 연동            WSL                    형상 관리를 위해 CLion과 Git을 연동했다. Git을 직접 설치한 후 경로를 잡아도 되지만, WSL을 이용하면 CLion에서 자동으로 WSL에 설치된 git path를 잡는다고 한다.           WSL 정리 -&gt; 추후 페이지 추가                       CLion에서 git 사용                    Alt + `                          ","categories": ["TIL"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/test/test/",
+        "url": "http://0.0.0.0:4000/til/TIL/",
+        "teaser": null
+      },{
+        "title": "[TIL] 2020-10-21",
+        "excerpt":"docker 설치 후 wsl과 연동 docker에서 jekyll 엔진을 구동하기 위해서는 회사 개발 컴퓨터로 접속해야만 했다. 이제는 WSL을 통해 Windows에서도 docker 사용할 수 있다! docker 설치 후 docker setting에서 WSL 연동할 수 있도록 몇 가지 설정만 하면 되서 매우 편리하다. microsoft docs에 방법이 잘 정리되어 있다. Unix Network Programming 복습 C++로...","categories": ["TIL"],
+        "tags": ["TIL"],
+        "url": "http://0.0.0.0:4000/til/TIL/",
         "teaser": null
       }]
